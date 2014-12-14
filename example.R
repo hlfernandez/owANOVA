d93fd929ce447adab646f8b94e86d2b18ec2daa0
Hugo López Fernández <hlfernandez@uvigo.es>
2014-12-14 11:34:00 +0100

    Fix example.R

--- example.R
+++ example.R
@@ -5,7 +5,7 @@ confLevel <- 0.95
 result <- owAnova(read.csv("data/testDataOneWay.csv"))
 print(result)
 
-if(getPValue(result) < (1-confLevel)) {
+if(result@pValue < (1-confLevel)) {
   sheffeResult <- scheffe(result, confLevel)
   print(sheffeResult)
 }
